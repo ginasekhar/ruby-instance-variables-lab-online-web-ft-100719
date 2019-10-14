b699a124c5dcd613c3a045e5846e602d2ed0fc5b
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-14 15:35:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,10 +5,11 @@ class Dog
  
   def name
     this_dogs_name
+    lassie = Dog.new
+    lassie.name = "Lassie"
+ 
+   lassie.name #=> "Lassie"
   end
 end
 
-lassie = Dog.new
-lassie.name = "Lassie"
- 
-lassie.name #=> "Lassie"
+l
